2c044b671810987e979b9369d41a174feddd84cc
Cory Bloor <cgbloor@ucalgary.ca>
2019-09-26 23:34:57 -0600

    Cleanup terminal escape codes

--- misc/terminal.h
+++ misc/terminal.h
@@ -25,7 +25,9 @@ typedef void(*wshandler_t)(int);
 int get_window_size(int* rows, int* cols);
 int set_window_size_handler(wshandler_t fn);
 
-// color codes
+// ANSI color codes
+// https://en.wikipedia.org/wiki/ANSI_escape_code
+#define T_BLACK   "\x1B[30m"
 #define T_RED     "\x1B[31m"
 #define T_GREEN   "\x1B[32m"
 #define T_YELLOW  "\x1B[33m"
@@ -33,4 +35,14 @@ int set_window_size_handler(wshandler_t fn);
 #define T_MAGENTA "\x1B[35m"
 #define T_CYAN    "\x1B[36m"
 #define T_WHITE   "\x1B[37m"
+
+#define T_BG_BLACK   "\x1B[40m"
+#define T_BG_RED     "\x1B[41m"
+#define T_BG_GREEN   "\x1B[42m"
+#define T_BG_YELLOW  "\x1B[43m"
+#define T_BG_BLUE    "\x1B[44m"
+#define T_BG_MAGENTA "\x1B[45m"
+#define T_BG_CYAN    "\x1B[46m"
+#define T_BG_WHITE   "\x1B[47m"
+
 #define T_RESET   "\x1B[0m"
