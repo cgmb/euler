39cd2f050c8968657ab11db70d27cd921d821223
Cory Bloor <cgbloor@ucalgary.ca>
2020-01-25 15:45:17 -0700

    Add OSX build and fix errors with clang

--- math/vec2f.h
+++ math/vec2f.h
@@ -5,25 +5,25 @@ typedef struct vec2f {
   float y;
 } vec2f;
 
-inline vec2f mulf_v2f(float s, vec2f v) {
+static inline vec2f mulf_v2f(float s, vec2f v) {
   v.x *= s;
   v.y *= s;
   return v;
 }
 
-inline vec2f add_v2f(vec2f a, vec2f b) {
+static inline vec2f add_v2f(vec2f a, vec2f b) {
   a.x += b.x;
   a.y += b.y;
   return a;
 }
 
-inline vec2f sub_v2f(vec2f a, vec2f b) {
+static inline vec2f sub_v2f(vec2f a, vec2f b) {
   a.x -= b.x;
   a.y -= b.y;
   return a;
 }
 
-inline vec2f make_v2f(float x, float y) {
+static inline vec2f make_v2f(float x, float y) {
   vec2f v = {x, y};
   return v;
 }
--- misc/cmp.h
+++ misc/cmp.h
@@ -2,22 +2,22 @@
 
 #include <stdint.h>
 
-inline float min_f(float a, float b) {
+static inline float min_f(float a, float b) {
   return a < b ? a : b;
 }
 
-inline float max_f(float a, float b) {
+static inline float max_f(float a, float b) {
   return a < b ? b : a;
 }
 
-inline int min_i(int a, int b) {
+static inline int min_i(int a, int b) {
   return a < b ? a : b;
 }
 
-inline int max_i(int a, int b) {
+static inline int max_i(int a, int b) {
   return a < b ? b : a;
 }
 
-inline uint8_t min_u8(uint8_t a, uint8_t b) {
+static inline uint8_t min_u8(uint8_t a, uint8_t b) {
   return a < b ? a : b;
 }
